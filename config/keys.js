2640c596a0eff4d3690e61a73f74c58c97903f1e
codingChewie <matthewtimothy910@yahoo.com>
2019-11-09 14:51:53 -0600

    init

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// const dbPassword = `mongodb+srv://YOUR_USERNAME_HERE:${encodeURIComponent(
+//   'YOUR_PASSWORD_HERE',
+// )}@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true`
+
+// local database
+const dbPassword = 'mongodb://localhost:27017/foobar'
+
+module.exports = {
+  mongoURI: dbPassword,
+}
